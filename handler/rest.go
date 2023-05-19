98f5a9e919bc719c8e3b67ab6ae25fadef6901a4
audeta35 <audeta35@gmail.com>
2023-05-19 14:46:26 +0700

    initial commit

--- handler/rest.go
+++ handler/rest.go
@@ -1,26 +1,60 @@
 package handler
 
 import (
+	"database/sql"
 	"net/http"
-	"sample/models"
+	"steradian_be/models"
 
 	"github.com/labstack/echo/v4"
 )
 
-type ArticleHandler struct {
+type ErrorResponse struct {
+	Message string `json:"message"`
 }
 
-func InitArticle() ArticleHandler {
-	return ArticleHandler{}
+type ArticleHandler struct {
+	DB *sql.DB
+}
+
+func InitArticle(db *sql.DB) ArticleHandler {
+	return ArticleHandler{
+		DB: db,
+	}
 }
 
 func (h ArticleHandler) FetchArticles(c echo.Context) (err error) {
-	datas := []models.Article{
-		models.Article {
-			ID: "1"
-			Title: "Hello World! This is Title"
-			Body: "Hello World! This is Body"
+	datas := make([]models.Article, 0)
+	query := `SELECT * FROM article`
+
+  rows, err := h.DB.Query(query)
+
+	if err != nil {
+		resp := ErrorResponse {
+			Message: err.Error(),
 		}
+
+		return c.JSON(http.StatusInternalServerError, resp)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var item models.Article
+		err := rows.Scan(
+			&item.ID,
+			&item.Title,
+			&item.Body,
+		)
+
+		if err != nil {
+			resp := ErrorResponse {
+				Message: err.Error(),
+			}
+
+			return c.JSON(http.StatusInternalServerError, resp)
+		}
+
+		datas = append(datas, item)
 	}
 
 	return c.JSON(http.StatusOK, datas)
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
 import (
-	httpHandler
+	"database/sql"
+	"log"
+	httpHandler "steradian_be/handler"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
 )
+
+func main() {
+	dbHost := "localhost"
+	dbPort := "3306"
+	dbUser := "root"
+	dbPass := ""
+	dbName := "steradian"
+
+	dsn := dbUser + `:` + dbPass + `@tcp(` + dbHost + `:` + dbPort + `)/` + dbName + `?parseTime=1&loc=Asia%2FJakarta`
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := httpHandler.InitArticle(db);
+	echoServer := echo.New();
+	// Register the handler
+	echoServer.GET("/articles", handler.FetchArticles)
+
+	// Start the server
+	echoServer.Start(":9090")
+}
